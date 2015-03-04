240f58e8c9469d87db2c93ecea14aa91aa166236
Laurent Cobos <laurent@11factory.fr>
2015-03-04 13:44:07 +0100

    fixup tests

--- test/app_model_test.rb
+++ test/app_model_test.rb
@@ -5,42 +5,42 @@ require 'ar_2_uml/app_model'
 class Ar2Uml::AppModelsTest < Minitest::Test
 
   def test_get_belonging_models
-    belonging_models = Ar2Uml::AppModel.new(T1).belonging_models
+    belonging_models = Ar2Uml::AppModel.new(Tt1).belonging_models
     assert_equal(2, belonging_models.count)
   end
-  
+
   def test_get_belonging_informations
-    belonging_models = Ar2Uml::AppModel.new(T2).belonging_models
-    assert_equal({t1:Ar2Uml::AppModelsTest::T1}, belonging_models.first)
+    belonging_models = Ar2Uml::AppModel.new(Tt2).belonging_models
+    assert_equal({tt1:Ar2Uml::AppModelsTest::Tt1}, belonging_models.first)
   end
   
   def test_get_aliased_belonging
-    belonging_models = Ar2Uml::AppModel.new(T3).belonging_models
-    assert_equal({foo:Ar2Uml::AppModelsTest::T1}, belonging_models.first)
+    belonging_models = Ar2Uml::AppModel.new(Tt3).belonging_models
+    assert_equal({foo:Ar2Uml::AppModelsTest::Tt1}, belonging_models.first)
   end
-  
+
   def test_get_table_name
-    assert_equal("t1s", Ar2Uml::AppModel.new(T1).table_name)
+    assert_equal("tt1s", Ar2Uml::AppModel.new(Tt1).table_name)
   end
-  
+
   def test_get_attributes
-    T1.stubs(columns: [
-      OpenStruct.new(type: :integer, name: :foo), 
+    Tt1.stubs(columns: [
+      OpenStruct.new(type: :integer, name: :foo),
       OpenStruct.new(type: :string, name: :bar)
     ])
-    assert_equal({foo: :integer, bar: :string}, Ar2Uml::AppModel.new(T1).model_attributes)
+    assert_equal({foo: :integer, bar: :string}, Ar2Uml::AppModel.new(Tt1).model_attributes)
   end
   
-  class T1 < ActiveRecord::Base
-    belongs_to :t2
-    belongs_to :t1
+  class Tt1 < ActiveRecord::Base
+    belongs_to :tt2
+    belongs_to :tt1
   end
   
-  class T2 < ActiveRecord::Base
-    belongs_to :t1
+  class Tt2 < ActiveRecord::Base
+    belongs_to :tt1
   end
   
-  class T3 < ActiveRecord::Base
-    belongs_to :foo, class_name:"T1"
+  class Tt3 < ActiveRecord::Base
+    belongs_to :foo, class_name:"Tt1"
   end
 end
--- test/app_models_repository_test.rb
+++ test/app_models_repository_test.rb
@@ -3,9 +3,14 @@ require 'ar_2_uml/app_models_repository'
 
 class Ar2Uml::AppModelsRepositoryTest < Minitest::Test
 
+  def setup
+    ObjectSpace.garbage_collect
+  end
+  
   def test_get_all_classes_in_object_space_that_looks_like_an_active_record_class
     repository = Ar2Uml::AppModelsRepository.new
-    assert_equal([T1, T2].map(&:to_s).sort, repository.all.map(&:to_s).sort)
+    assert(repository.all.include?(T1))
+    assert(repository.all.include?(T2))
   end
 
   class T1
